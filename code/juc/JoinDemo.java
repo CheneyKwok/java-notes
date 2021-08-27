51622c14cc0efb276cd8db2757f5a6a80c3e4e0e
郭志成 <2399214024@qq.com>
2021-08-27 20:34:34 +0800

    Create JoinDemo.java

---
+++ code/juc/JoinDemo.java
@@ -0,0 +1,32 @@
+package juc;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class JoinDemo {
+    static int r = 0;
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        log.debug("start");
+        Thread t1 = new Thread(() -> {
+            log.debug("start");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.debug("end");
+            r = 10;
+        });
+
+        t1.start();
+        log.debug("res: {}", r);
+        log.debug("end");
+    }
+}
