6615e9d04f42722cb7cbf60c622dbadad6f383a9
郭志成 <2399214024@qq.com>
2021-09-11 18:15:02 +0800

    Create InterruptParkDemo.java

---
+++ notes/juc/code/InterruptParkDemo.java
@@ -0,0 +1,27 @@
+package juc.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+@Slf4j
+public class InterruptParkDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            log.info("park.....");
+            LockSupport.park();
+            log.info("un park......");
+            log.info("打断状态：{}", Thread.currentThread().isInterrupted());
+//            log.info("打断状态：{}", Thread.interrupted());
+            LockSupport.park();
+            log.info("un park......");
+        });
+
+        t.start();
+        TimeUnit.SECONDS.sleep(1);
+        t.interrupt();
+    }
+}
+
