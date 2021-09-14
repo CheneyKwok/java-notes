660b44b22b6ad8689b59a3384a472a89970b3ec9
郭志成 <2399214024@qq.com>
2021-09-14 18:53:02 +0800

    Create SynchronizedDemo.java

---
+++ notes/juc/code/SynchronizedDemo.java
@@ -0,0 +1,36 @@
+package juc.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SynchronizedDemo {
+
+    static int counter = 0;
+    static Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+                for (int i = 0; i < 5000000; i++) {
+                    synchronized (lock){
+                        log.info("{} ---- {}", Thread.currentThread().getName(), counter);
+
+                        counter++;
+                }
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+                for (int i = 0; i < 5000000; i++) {
+                    synchronized (lock) {
+                        log.info("{} ---- {}", Thread.currentThread().getName(), counter);
+                        counter--;
+                }
+            }
+        }, "t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        log.info("{}", counter);
+    }
+}
