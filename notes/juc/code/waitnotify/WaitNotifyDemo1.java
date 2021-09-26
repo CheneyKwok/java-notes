d8600ba2c4cdb58149af19445214d1840bc1704e
CheneyKwok <68406855+CheneyKwok@users.noreply.github.com>
2021-09-26 22:07:20 +0800

    update juc

---
+++ notes/juc/code/waitnotify/WaitNotifyDemo1.java
@@ -0,0 +1,47 @@
+package juc.code.waitnotify;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static juc.code.Sleeper.sleep;
+
+
+@Slf4j
+public class WaitNotifyDemo1 {
+
+    final static Object obj = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (obj){
+                log.info("执行...");
+                try {
+                    obj.wait();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                log.info("其他代码...");
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            synchronized (obj){
+                log.info("执行...");
+                try {
+                    obj.wait();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                log.info("其他代码...");
+            }
+        }, "t2").start();
+
+        sleep(2);
+        log.info("唤醒 obj 上其他线程");
+        synchronized (obj) {
+            // 唤醒 obj 上一个线程
+//            obj.notify();
+            // 唤醒 obj 上全部线程
+            obj.notifyAll();
+        }
+    }
+}
