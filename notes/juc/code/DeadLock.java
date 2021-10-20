cd99f310ca01ff69eadfa6ed9e393d6f7eb45417
郭志成 <2399214024@qq.com>
2021-10-20 17:48:11 +0800

    update juc

---
+++ notes/juc/code/DeadLock.java
@@ -0,0 +1,30 @@
+package juc.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DeadLock {
+    public static void main(String[] args) {
+        Object A = new Object();
+        Object B = new Object();
+        new Thread(() -> {
+            synchronized (A){
+                log.info("lock A");
+                Sleeper.sleep(1);
+                synchronized (B){
+                    log.info("lock B");
+                }
+            }
+        }, "t1").start();
+        new Thread(() -> {
+            synchronized (B){
+                log.info("lock B");
+                Sleeper.sleep(0.5);
+                synchronized (A){
+                    log.info("lock A");
+                }
+            }
+        }, "t2").start();
+    }
+
+}
