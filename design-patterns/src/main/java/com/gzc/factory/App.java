a64dde9bde43625525c4e9f1d919ac122ddb444b
guozhicheng <2399214024@qq.com>
2022-01-21 20:38:57 +0800

    Update App.java

--- design-patterns/src/main/java/com/gzc/factory/App.java
+++ design-patterns/src/main/java/com/gzc/factory/App.java
@@ -2,6 +2,11 @@ package com.gzc.factory;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 静态简单工厂
+ * 提供一个静态方法来创建和返回不同类的对象，以隐藏实现逻辑并使客户端代码专注于使用而不是对象的初始化和管理。
+ * CoinFactory 是工厂类，它提供了一个静态方法来创建不同类型的硬币
+ */
 @Slf4j
 public class App {
 
