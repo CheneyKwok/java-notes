3874ce1944d65e44563b7cd0b19796f3f02d0bc0
CheneyKwok <2399214024@qq.com>
2022-05-17 21:02:30 +0800

    Netty: selector 处理写

---
+++ netty/src/main/java/com/gzc/nio/socket/WriteClient.java
@@ -0,0 +1,28 @@
+package com.gzc.nio.socket;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class WriteClient {
+
+    public static void main(String[] args) throws IOException {
+        SocketChannel sc = SocketChannel.open();
+        boolean connect = sc.connect(new InetSocketAddress(9999));
+        System.out.println("connect = " + connect);
+
+        int count = 0;
+        ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
+        while (true) {
+            int read = sc.read(buffer);
+            count += read;
+            System.out.println(count);
+            buffer.clear();
+            if (read == -1) {
+                break;
+            }
+        }
+    }
+}
---
+++ netty/src/main/java/com/gzc/nio/socket/WriteServer.java
@@ -0,0 +1,63 @@
+package com.gzc.nio.socket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+public class WriteServer {
+
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        ssc.bind(new InetSocketAddress(9999));
+        ssc.register(selector, SelectionKey.OP_ACCEPT, null);
+
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove();
+
+                if (key.isAcceptable()) {
+                    ssc = (ServerSocketChannel) key.channel();
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey scKey = sc.register(selector, 0, null);
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < 90000; i++) {
+                        builder.append("a");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(builder.toString());
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    for (int i = 0; i < 90000; i++) {
+                        builder.append("a");
+                    }
+                    buffer = Charset.defaultCharset().encode(builder.toString());
+                    if (buffer.hasRemaining()) {
+                        // 追加可写事件
+                        scKey.attach(buffer);
+                        scKey.interestOps(scKey.interestOps() | SelectionKey.OP_WRITE);
+                    }
+                }
+
+                if (key.isWritable()) {
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    if (!buffer.hasRemaining()) {
+                        // 移除
+                        sc.register(selector, key.interestOps() ^ SelectionKey.OP_WRITE, null);
+                        key.attach(null);
+                    }
+                }
+            }
+        }
+    }
+}
