42103976b49bf4077fd42aa7c680179443ad020d
CheneyKwok <68406855+CheneyKwok@users.noreply.github.com>
2022-05-20 00:01:45 +0800

    Netty：Hello Demo

---
+++ netty/src/main/java/com/gzc/netty/hello/HelleClient.java
@@ -0,0 +1,27 @@
+package com.gzc.netty.hello;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelleClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress(6666))
+                .sync()
+                .channel()
+                .writeAndFlush("hello, world");
+    }
+}
