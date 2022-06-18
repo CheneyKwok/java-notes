59ce68c0b0fdc5318259ca5f8b6091fbf6d89b8d
guozhicheng <2399214024@qq.com>
2022-06-18 17:59:45 +0800

    JVM：GC分析

--- jvm/src/main/java/com/gzc/gc/Demo1.java
+++ jvm/src/main/java/com/gzc/gc/Demo1.java
@@ -1,18 +1,20 @@
 package com.gzc.gc;
 
+import java.util.ArrayList;
+
 public class Demo1 {
     private static final int _512KB = 512 * 1024;
     private static final int _1MB = 1024 * 1024;
-
     private static final int _6MB = 6 * _1MB;
-
     private static final int _7MB = 7 * _1MB;
-
     private static final int _8MB = 8 * _1MB;
 
     // -Xms20M -Xmx20M -Xmn10M -XX:+UseSerialGC -XX:+PrintGCDetails -verbose:gc
     // XX:+UseSerialGC 使用指定的GC，不会去动态调整幸存区的大小
     public static void main(String[] args) {
-
+        ArrayList<byte[]> list = new ArrayList<>();
+        list.add(new byte[_7MB]);
+//        list.add(new byte[_512KB]);
+//        list.add(new byte[_512KB]);
     }
 }
