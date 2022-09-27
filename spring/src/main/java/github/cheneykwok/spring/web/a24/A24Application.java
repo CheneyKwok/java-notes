2ee6fab782b4249b32682964f5ad0bb488182e20
CheneyKwok <68406855+CheneyKwok@users.noreply.github.com>
2022-09-27 22:47:20 +0800

    Spring：@ControllerAdvice

---
+++ spring/src/main/java/github/cheneykwok/spring/web/a24/A24Application.java
@@ -0,0 +1,66 @@
+package github.cheneykwok.spring.web.a24;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.method.ControllerAdviceBean;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+/*
+    @InitBinder 的来源有两个
+    1. @ControllerAdvice 中 @InitBinder 标注的方法，由 RequestMappingHandlerAdapter 在初始化时解析并记录
+    2. @Controller 中 @InitBinder 标注的方法，由 RequestMappingHandlerAdapter 会在控制器方法首次执行时解析并记录
+ */
+@Slf4j
+public class A24Application {
+
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+        handlerAdapter.setApplicationContext(context);
+        handlerAdapter.afterPropertiesSet();
+        log.debug("start");
+        showBindMethods(handlerAdapter);
+
+        // 获取 getDataBinderFactory()
+        Method getDataBinderFactory = RequestMappingHandlerAdapter.class.getDeclaredMethod("getDataBinderFactory", HandlerMethod.class);
+        getDataBinderFactory.setAccessible(true);
+
+        log.debug("模拟调用 Controller1 的 foo 方法");
+        getDataBinderFactory.invoke(handlerAdapter, new HandlerMethod(new WebConfig.Controller1(), WebConfig.Controller1.class.getMethod("foo")));
+        showBindMethods(handlerAdapter);
+
+        log.debug("模拟调用 Controller2 的 foo 方法");
+        getDataBinderFactory.invoke(handlerAdapter, new HandlerMethod(new WebConfig.Controller2(), WebConfig.Controller2.class.getMethod("bar")));
+        showBindMethods(handlerAdapter);
+        context.close();
+    }
+
+    @SuppressWarnings("all")
+    private static void showBindMethods(RequestMappingHandlerAdapter handlerAdapter) throws NoSuchFieldException, IllegalAccessException {
+        Field initBinderAdviceCache = RequestMappingHandlerAdapter.class.getDeclaredField("initBinderAdviceCache");
+        initBinderAdviceCache.setAccessible(true);
+        Map<ControllerAdviceBean, Set<Method>> globalMap = (Map<ControllerAdviceBean, Set<Method>>) initBinderAdviceCache.get(handlerAdapter);
+        log.debug("全局的 @InitBinder 方法 {}",
+                globalMap.values().stream()
+                        .flatMap(ms -> ms.stream().map(m -> m.getName()))
+                        .collect(Collectors.toList())
+        );
+
+        Field initBinderCache = RequestMappingHandlerAdapter.class.getDeclaredField("initBinderCache");
+        initBinderCache.setAccessible(true);
+        Map<Class<?>, Set<Method>> controllerMap = (Map<Class<?>, Set<Method>>) initBinderCache.get(handlerAdapter);
+        log.debug("控制器的 @InitBinder 方法 {}",
+                controllerMap.entrySet().stream()
+                        .flatMap(e -> e.getValue().stream().map(v -> e.getKey().getSimpleName() + "." + v.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
---
+++ spring/src/main/java/github/cheneykwok/spring/web/a24/WebConfig.java
@@ -0,0 +1,50 @@
+package github.cheneykwok.spring.web.a24;
+
+import github.cheneykwok.spring.web.a23.MyDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@Configuration
+public class WebConfig {
+
+    @ControllerAdvice // 控制器增强类 三个功能：@ExceptionHandler、@InitBinder、@ModelAttribute
+    static class MyControllerAdvice {
+        @InitBinder
+        public void binder3(WebDataBinder webDataBinder) {
+            webDataBinder.addCustomFormatter(new MyDateFormatter("binder3 转换器"));
+        }
+    }
+
+    @Controller
+    static class Controller1 {
+        @InitBinder
+        public void binder1(WebDataBinder webDataBinder) {
+            webDataBinder.addCustomFormatter(new MyDateFormatter("binder1 转换器"));
+        }
+
+        public void foo() {
+
+        }
+    }
+
+    @Controller
+    static class Controller2 {
+        @InitBinder
+        public void binder21(WebDataBinder webDataBinder) {
+            webDataBinder.addCustomFormatter(new MyDateFormatter("binder21 转换器"));
+        }
+
+        @InitBinder
+        public void binder22(WebDataBinder webDataBinder) {
+            webDataBinder.addCustomFormatter(new MyDateFormatter("binder22 转换器"));
+        }
+
+        public void bar() {
+
+        }
+    }
+
+}
